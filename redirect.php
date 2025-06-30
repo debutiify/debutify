6d28bfa2290f630357dd8e53d637d4eb28175650
getfunnelish <digitalsales647@gmail.com>
2025-06-30 11:04:44 +0530

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -1,1 +1,11 @@
-
+// redirect.php
+<?php
+$url = $_GET['url'];
+?>
+<html>
+<head>
+  <meta name="referrer" content="no-referrer">
+  <meta http-equiv="refresh" content="0;url=<?php echo htmlspecialchars($url); ?>">
+</head>
+<body></body>
+</html>
